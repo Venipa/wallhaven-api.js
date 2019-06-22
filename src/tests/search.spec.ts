396df425812b8a39935e1e733c0b82cec74809f9
Venipa <admin@venipa.net>
2019-06-22 21:35:01 +0200

    fixed lint, tests now working

--- src/tests/search.spec.ts
+++ src/tests/search.spec.ts
@@ -8,3 +8,10 @@ test('search', async t => {
   });
   t.is(data.length > 0, true, `Data Entries: ${data.length}`);
 });
+
+test('random', async t => {
+  const client = new Wallhaven();
+  const requestPrev = await client.random('Naruto');
+  const requestNext = await client.random('Tokyo Ghoul');
+  t.is(requestNext.data[0].id !== requestPrev.data[0].id, true);
+});
